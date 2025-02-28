0fd1154d3ecfa070dc52dbe505c6b29e5677325c
Justin Nesselrotte <admin@jnesselr.org>
2025-02-27 20:40:31 -0700

    Added setup.py file so project can be imported

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+VERSION = "1.0.0"
+
+
+def readme():
+    with open('README.md', encoding="utf8") as f:
+        return f.read()
+
+
+setup(
+    name="card-automation-server",
+    version=VERSION,
+    description="WinDSX Card Automation Server",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    author="Justin Nesselrotte",
+    author_email="admin@jnesselr.org",
+    license="MIT",
+    url="https://github.com/card-automation/card-server",
+    install_requires=[
+        "githubkit",
+        "platformdirs",
+        "psutil",
+        "pyodbc",
+        "pytest",
+        "requests",
+        "sentry-sdk==2.8.0",
+        "SQLAlchemy",
+        "sqlalchemy-access",
+        "tomlkit",
+        "watchdog",
+    ],
+    packages=["card_automation_server"],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ]
+)
