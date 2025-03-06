7d37b3a67b3da578edf2b465e7be184fe650c2a5
Justin Nesselrotte <admin@jnesselr.org>
2025-03-06 10:54:21 -0700

    Listening to Raw Comm Server Message for card scanned event

--- card_automation_server/workers/card_scan_watcher.py
+++ card_automation_server/workers/card_scan_watcher.py
@@ -1,14 +1,17 @@
+from datetime import datetime
+
 from sqlalchemy import select, func
 from sqlalchemy.orm import Session
 
 from card_automation_server.plugins.types import CardScanEventType, CardScan
-from card_automation_server.windsx.db.models import EvnLog
+from card_automation_server.windsx.db.models import EvnLog, NAMES, CARDS
 from card_automation_server.windsx.engines import LogEngine
-from card_automation_server.workers.events import LogDatabaseUpdated, CardScanned
+from card_automation_server.workers.events import LogDatabaseUpdated, CardScanned, RawCommServerMessage
 from card_automation_server.workers.utils import EventsWorker
 
 _Events = [
-    LogDatabaseUpdated
+    LogDatabaseUpdated,
+    RawCommServerMessage,
 ]
 
 
@@ -22,10 +25,14 @@ class CardScanWatcher(EventsWorker[LogDatabaseUpdated]):
         if isinstance(event, LogDatabaseUpdated):
             self._handle_log_database_update(event)
 
+        if isinstance(event, RawCommServerMessage):
+            self._handle_raw_comm_server_message(event)
+
     def _handle_log_database_update(self, _: LogDatabaseUpdated):
         latest_events = self._log_session.scalars(
             select(EvnLog)
             .where(EvnLog.TimeDate > self._last_timestamp)
+            .order_by(EvnLog.TimeDate)
         ).all()
 
         event: EvnLog
@@ -47,6 +54,52 @@ class CardScanWatcher(EventsWorker[LogDatabaseUpdated]):
                 )
             )
 
-            # We didn't order the event log for speed, so this just ensures processing the events out of order still
-            # gets us the latest timestamp.
+            # This ensures processing the events out of order still gets us the latest timestamp.
             self._last_timestamp = max(self._last_timestamp, event.TimeDate)
+
+    def _handle_raw_comm_server_message(self, message: RawCommServerMessage):
+        if message.type != 1:
+            return  # We only handle log events for card scans
+
+        if len(message.data) < 7:
+            return  # Can't get the event type to see if it's one we care about
+
+        event_type = message.data[6]
+
+        if event_type not in [x.value for x in CardScanEventType]:
+            return  # Not a card scan event, safe to ignore
+
+        timestamp = datetime(
+            year=message.data[10],
+            month=message.data[11],
+            day=message.data[12],
+            hour=message.data[13],
+            minute=message.data[14],
+            second=message.data[15],
+        )
+
+        if timestamp < self._last_timestamp:
+            return  # Probably already seen it via DB update, ignore it (though this one is always faster)
+
+        location_id = message.data[2]
+        device_id = message.data[3]
+        card_number = message.data[21]
+
+        name_id = self._log_session.scalar(
+            select(NAMES.ID)
+            .join(CARDS, CARDS.NameID == NAMES.ID)
+            .where(CARDS.Code == float(card_number))
+        )
+
+        self._outbound_event_queue.put(
+            CardScanned(
+                card_scan=CardScan(
+                    name_id=name_id,
+                    card_number=card_number,
+                    scan_time=timestamp,
+                    device=device_id,
+                    event_type=CardScanEventType(event_type),
+                    location_id=location_id
+                )
+            )
+        )
