3f49974f5eb3ac9cbe2cbd858593e65604fea6ab
Justin Nesselrotte <admin@jnesselr.org>
2025-06-29 01:58:36 -0600

    Added specific test for RawCommServerEvent -> CardScanned

--- tests/workers/test_card_scan_watcher.py
+++ tests/workers/test_card_scan_watcher.py
@@ -9,7 +9,8 @@ from card_automation_server.ioc import Resolver
 from card_automation_server.plugins.types import CommServerEventType
 from card_automation_server.windsx.db.models import EvnLog
 from card_automation_server.workers.card_scan_watcher import CardScanWatcher
-from card_automation_server.workers.events import CardScanned, LogDatabaseUpdated
+from card_automation_server.workers.events import CardScanned, LogDatabaseUpdated, RawCommServerEvent, \
+    RawCommServerMessage
 from tests.conftest import main_location_id
 
 
@@ -50,6 +51,34 @@ class TestCardScanWatcher:
         log_session.commit()
 
         card_scan_watcher.event(LogDatabaseUpdated())
+
+        with card_scan_watcher.outbound_queue.not_empty:
+            card_scan_watcher.outbound_queue.not_empty.wait(3)
+
+        assert card_scan_watcher.outbound_queue.qsize() == 1
+
+        event = card_scan_watcher.outbound_queue.get()
+
+        assert isinstance(event, CardScanned)
+        assert event.card_scan.scan_time == scan_time
+        assert event.card_scan.card_number == 3000
+        assert event.card_scan.event_type == CommServerEventType.ACCESS_GRANTED
+        assert event.card_scan.name_id == 101
+        assert event.card_scan.device == 0
+
+    def test_raw_comm_server_event_emits_card_scanned_event(self, card_scan_watcher: CardScanWatcher):
+        # Taken from a real event and then modified. Unimportant fields were removed/modified. Everything else should
+        # match our conftest.py data.
+        scan_time = datetime(2025, 1, 2, 3, 4, 5)
+        message = RawCommServerMessage.parse(
+            "1 48 3 0 -1 0 8 0 0 1 2025 1 2 3 4 5 0 0 0 0 0 3000 82 0 "
+            "*3/4/2025 3:4:5 AM .. (3) .. Access Granted .. Main Door .. BuildingManager, BobThe"
+        )
+
+        event = message.event
+        assert event is not None
+
+        card_scan_watcher.event(event)
 
         with card_scan_watcher.outbound_queue.not_empty:
             card_scan_watcher.outbound_queue.not_empty.wait(3)
